d4fea0275fb04ef293dd2f0798f5d3fc20ce0cb2
zakcrust <zakcrusted@gmail.com>
2021-10-01 14:22:22 +0700

    add some routes

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,51 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['middleware' => 'auth', 'prefix' => 'api'], function ($router) {
+
     $router->get('me', 'AuthController@me');
+
+    $router->group(['prefix' => 'gallery'], function () use ($router) {
+        $router->get('', 'GalleriesController@get');
+        $router->post('', 'GalleriesController@create');
+        $router->put('update/{id}', 'GalleriesController@update');
+        $router->delete('delete/{id}', 'GalleriesController@delete');
+    });
+
+    $router->group(['prefix' => 'agenda'], function () use ($router) {
+        $router->get('', 'AgendasController@get');
+        $router->post('', 'AgendasController@create');
+        $router->put('update/{id}', 'AgendasController@update');
+        $router->delete('delete/{id}', 'AgendasController@delete');
+    });
+
+    $router->group(['prefix' => 'chat'], function () use ($router) {
+        $router->get('', 'ChatsController@get');
+        $router->post('', 'ChatsController@create');
+        $router->put('endchat/{id}', 'ChatsController@endchat');
+        $router->delete('delete/{id}', 'ChatsController@delete');
+    });
+
+    $router->group(['prefix' => 'agenda_detail'], function () use ($router) {
+        $router->get('', 'AgendaDetailsController@get');
+        $router->post('', 'AgendaDetailsController@create');
+        $router->put('update/{id}', 'AgendaDetailsController@update');
+        $router->delete('delete/{id}', 'AgendaDetailsController@delete');
+    });
 
     $router->group(['prefix' => 'user'], function () use ($router) {
         $router->get('', 'UserController@get');
+    });
+
+    $router->group(['prefix' => 'message_attachments'], function () use ($router) {
+        $router->get('', 'MessageAttachments@get');
+        $router->post('', 'MessageAttachments@create');
+        $router->delete('delete/{id}', 'MessageAttachments@delete');
+    });
+
+    $router->group(['prefix' => 'message_receivers'], function () use ($router) {
+        $router->get('', 'MessageReceivers@get');
+        $router->put('update/{id}', 'MessageReceivers@update');
+        $router->delete('delete/{id}', 'MessageReceivers@delete');
     });
 
     $router->group(['prefix' => 'message'], function () use ($router) {
@@ -27,6 +68,13 @@ $router->group(['middleware' => 'auth', 'prefix' => 'api'], function ($router) {
         $router->post('', 'MessagesController@create');
         $router->put('update/{id}', 'MessagesController@update');
         $router->delete('delete/{id}', 'MessagesController@delete');
+    });
+
+    $router->group(['prefix' => 'document_type'], function () use ($router) {
+        $router->get('', 'DocumentTypesController@get');
+        $router->post('', 'DocumentTypesController@create');
+        $router->put('update/{id}', 'DocumentTypesController@update');
+        $router->delete('delete/{id}', 'DocumentTypesController@delete');
     });
 
     $router->group(['prefix' => 'documents'], function () use ($router) {
