94993a566dada69a3959e74247d370bec35f579e
aditrio <30noadi@gmail.com>
2021-10-03 14:42:04 +0700

    test cors 2

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [    
    'paths' => ['*'],
    'allowed_methods' => ['*'],
-   'allowed_origins' => ['*'],
+   'allowed_origins' => ['http://localhost:8080'],
    'allowed_origins_patterns' => [],
    'allowed_headers' => ['*'],
    'exposed_headers' => [],
