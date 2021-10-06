95cdbd912f9a5789b7ced5cab898db8f8e03738c
zakcrust <zakcrusted@gmail.com>
2021-10-06 14:10:03 +0700

    change removing method of role opd on role update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -125,7 +125,10 @@ class RoleController extends Controller
             $role->save();
 
             if ($role) {
-                $opds = RolesOpds::where('role_id', $role->id)->delete();
+                $opds = RolesOpds::where('role_id', $role->id)->get();
+                foreach ($opds as $opd) {
+                    $opd->delete();
+                }
                 
                 if($request->input('opds'))
                 {
