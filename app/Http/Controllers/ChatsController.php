c420a0f15605d14cf2cd1a33eefd6b30b5ac983e
zakcrust <zakcrusted@gmail.com>
2021-11-06 17:02:31 +0700

    fix get chat 2

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -175,7 +175,7 @@ class ChatsController extends Controller
             foreach ($chat as $chat_sender) {
                 if (count($data) > 0)
                     foreach ($data as $_data) {
-                        return $_data;
+                        return $_data['id'];
                         if ($key)
                             break;
                     }
