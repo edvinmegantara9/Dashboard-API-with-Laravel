bda9da84e6d552fc1ff33154538119d73ec97aa6
zakcrust <zakcrusted@gmail.com>
2021-11-06 16:02:00 +0700

    fix get chat

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -173,7 +173,7 @@ class ChatsController extends Controller
             foreach ($chat as $chat_sender) {
                 if ($data != [])
                     foreach ($data as $_data) {
-                        dd($_data);
+                        dd(array_values($_data));
                         $key = $chat->id == $_data->id;
                         if ($key)
                             break;
