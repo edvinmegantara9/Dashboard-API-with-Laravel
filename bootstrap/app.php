11c24c0e5f8515e412b83cf3e3170cea8b36266e
zakcrust <zakcrusted@gmail.com>
2021-11-06 18:42:51 +0700

    remove laravel excel registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -100,7 +100,7 @@ $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Chuckrincon\LumenConfigDiscover\DiscoverServiceProvider::class);
-$app->register(Maatwebsite\Excel\ExcelServiceProvider::class);
+// $app->register(Maatwebsite\Excel\ExcelServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
