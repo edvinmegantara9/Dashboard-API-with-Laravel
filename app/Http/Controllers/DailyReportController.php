1719207ff998ed5ccb9607d72cec101c56c0e80f
zakcrust <zakcrusted@gmail.com>
2021-12-19 13:02:12 +0700

    change export excel data

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -39,20 +39,20 @@ class DailyReportController extends Controller
         // )->where('date', '>=', DATE($firstdate))->where('date', '<=', DATE($lastdate))->get();
         $dailyReport = DB::table('daily_reports')
         ->select(
-            'CONVERT(daily_reports.created_at, DATE)',
-            'CONVERT(daily_reports.created_at, TIME)',
+            DB::raw('CONVERT(daily_reports.created_at, DATE) as date'),
+            DB::raw('CONVERT(daily_reports.created_at, TIME) as time'),
             'daily_reports.name',
-            'CONVERT(daily_reports.nip, NCHAR)',
+            DB::raw('CONVERT(daily_reports.nip, NCHAR) as nip'),
             'users.position',
             'users.group',
             'daily_reports.report'
         )
         ->join('users', 'daily_reports.nip', '=', 'users.nip')
         ->get();
-        // foreach ($dailyReport as $report) {
-        //     $report->date = Carbon::createFromFormat('Y-m-d H:i:s', $report->date)->format('Y.m.d');
-        //     $report->time = Carbon::createFromFormat('Y-m-d H:i:s', $report->time)->format('H:i:s');
-        // }
+        foreach ($dailyReport as $report) {
+            $report->date = Carbon::createFromFormat('Y-m-d H:i:s', $report->date)->format('Y.m.d');
+            $report->time = Carbon::createFromFormat('Y-m-d H:i:s', $report->time)->format('H:i:s');
+        }
         Excel::store(new DailyReportExport($dailyReport), 'daily_report.xlsx');
         return response()->download(storage_path("app/daily_report.xlsx"), "daily_report.xlsx", ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Methods" => "GET, POST, PUT, DELETE, OPTIONS"]);
     }
