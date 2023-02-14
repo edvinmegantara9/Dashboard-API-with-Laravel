6df3f95c37f75297bf19dcf80d91f04ec750e1c3
Seno Tresna <seno.tresna@gmail.com>
2023-02-14 13:25:34 +0700

    memperbaiki migrasi (sblmnya erorr tidak nullable)

--- database/migrations/2023_02_10_063708_add_verificator_to_users_table.php
+++ database/migrations/2023_02_10_063708_add_verificator_to_users_table.php
@@ -14,7 +14,7 @@ class AddVerificatorToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('verificator');
+            $table->integer('verificator')->nullable();
         });
     }
 
