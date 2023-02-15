751e3820fdf1a1574ab2f82375df8901312305a8
edvinmegantara9 <edvin.megantara9@gmail.com>
2023-02-15 18:21:11 +0700

    update

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\DocumentAttachment;
 use App\Models\DocumentSupport;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 
@@ -75,7 +76,6 @@ class DocumentController extends Controller
 
     public function create(Request $request)
     {
-
         $this->validate($request,[
             'document_type' => 'required',
             'tittle' => 'required',
@@ -115,112 +115,108 @@ class DocumentController extends Controller
             'document_attachments.*.margin_right' => 'required|integer',
             
             'document_supports.*.file' => 'required',
-            
-
         ]);
 
-    try{
-        DB::beginTransaction();
-        $doc = new Document;
-        $doc->document_type = $request->input('document_type');
-        $doc->tittle = $request->input('tittle');
-        $doc->signer = $request->input('signer');
-        $doc->user_id = Auth::user()->id;
-        $doc->date = $request->input('date');
-        $doc->status = $request->input('status');
-        $doc->legal_drafter = $request->input('legal_drafter');
-        $doc->save();
+        try{
+            DB::beginTransaction();
+            $doc = new Document;
+            $doc->document_type = $request->input('document_type');
+            $doc->tittle = $request->input('tittle');
+            $doc->signer = $request->input('signer');
+            $doc->user_id = Auth::user()->id;
+            $doc->date = $request->input('date');
+            $doc->status = $request->input('status');
+            $doc->legal_drafter = $request->input('legal_drafter');
+            $doc->save();
 
-        if (count($request->get('document_considers')) > 0) {
-            foreach ($request->get('document_considers') as $d) {
-                $consider = new DocumentConsider;
-                $consider->document_id = $doc->id;
-                $consider->description = $d['description'];
-                $consider->margin_top =$d['margin_top'];
-                $consider->margin_bottom =$d['margin_bottom'];
-                $consider->margin_left =$d['margin_left'];
-                $consider->margin_right =$d['margin_right'];
-                $consider->save();
+            if (count($request->get('document_considers')) > 0) {
+                foreach ($request->get('document_considers') as $d) {
+                    $consider = new DocumentConsider;
+                    $consider->document_id = $doc->id;
+                    $consider->description = $d['description'];
+                    $consider->margin_top =$d['margin_top'];
+                    $consider->margin_bottom =$d['margin_bottom'];
+                    $consider->margin_left =$d['margin_left'];
+                    $consider->margin_right =$d['margin_right'];
+                    $consider->save();
+                }
             }
-        }
 
-        if (count($request->get('document_remembers')) > 0) {
-            foreach ($request->get('document_remembers') as $d) {
-                $remembers = new DocumentRemember;
-                $remembers->document_id = $doc->id;
-                $remembers->description = $d['description'];
-                $remembers->margin_top =$d['margin_top'];
-                $remembers->margin_bottom =$d['margin_bottom'];
-                $remembers->margin_left =$d['margin_left'];
-                $remembers->margin_right =$d['margin_right'];
-                $remembers->save();
+            if (count($request->get('document_remembers')) > 0) {
+                foreach ($request->get('document_remembers') as $d) {
+                    $remembers = new DocumentRemember;
+                    $remembers->document_id = $doc->id;
+                    $remembers->description = $d['description'];
+                    $remembers->margin_top =$d['margin_top'];
+                    $remembers->margin_bottom =$d['margin_bottom'];
+                    $remembers->margin_left =$d['margin_left'];
+                    $remembers->margin_right =$d['margin_right'];
+                    $remembers->save();
+                }
             }
-        }
-        if (count($request->get('document_notices')) > 0) {
-            foreach ($request->get('document_notices') as $d) {
-                $notices = new DocumentNotice;
-                $notices->document_id = $doc->id;
-                $notices->description = $d['description'];
-                $notices->margin_top =$d['margin_top'];
-                $notices->margin_bottom =$d['margin_bottom'];
-                $notices->margin_left =$d['margin_left'];
-                $notices->margin_right =$d['margin_right'];
-                $notices->save();
+            if (count($request->get('document_notices')) > 0) {
+                foreach ($request->get('document_notices') as $d) {
+                    $notices = new DocumentNotice;
+                    $notices->document_id = $doc->id;
+                    $notices->description = $d['description'];
+                    $notices->margin_top =$d['margin_top'];
+                    $notices->margin_bottom =$d['margin_bottom'];
+                    $notices->margin_left =$d['margin_left'];
+                    $notices->margin_right =$d['margin_right'];
+                    $notices->save();
+                }
             }
-        }
-        $status = new DocumentStatus;
-        $status->document_id = $doc->id;
-        $status->status = $request->input('document_statuses.status');
-        $status->remark = $request->input('document_statuses.remark');
-        $status->user_id = Auth::user()->id;
-        $status->save();
+            $status = new DocumentStatus;
+            $status->document_id = $doc->id;
+            $status->status = $request->input('document_statuses.status');
+            $status->remark = $request->input('document_statuses.remark');
+            $status->user_id = Auth::user()->id;
+            $status->save();
 
-        if (count($request->get('document_attachments')) > 0) {
-            foreach ($request->get('document_attachments') as $d) {
-                $attachment = new DocumentAttachment();
-                $attachment->document_id = $doc->id;
-                $attachment->tittle = $d['tittle'];
-                $attachment->description = $d['description'];
-                $attachment->margin_top =$d['margin_top'];
-                $attachment->margin_bottom =$d['margin_bottom'];
-                $attachment->margin_left =$d['margin_left'];
-                $attachment->margin_right =$d['margin_right'];
-                $attachment->save();
-                          
+            if (count($request->get('document_attachments')) > 0) {
+                foreach ($request->get('document_attachments') as $d) {
+                    $attachment = new DocumentAttachment();
+                    $attachment->document_id = $doc->id;
+                    $attachment->tittle = $d['tittle'];
+                    $attachment->description = $d['description'];
+                    $attachment->margin_top =$d['margin_top'];
+                    $attachment->margin_bottom =$d['margin_bottom'];
+                    $attachment->margin_left =$d['margin_left'];
+                    $attachment->margin_right =$d['margin_right'];
+                    $attachment->save();
+                            
+                }
             }
-        }
 
-        if (count($request->get('document_supports')) > 0) {
-            foreach ($request->get('document_supports') as $d) {
-                $suport = new DocumentSupport();
-                $suport->document_id = $doc->id;
-                $suport->file = $d['file'];    
-                $suport->save();            
+            if (count($request->get('document_supports')) > 0) {
+                foreach ($request->get('document_supports') as $d) {
+                    $suport = new DocumentSupport();
+                    $suport->document_id = $doc->id;
+                    $suport->file = $d['file'];    
+                    $suport->save();            
+                }
             }
-        }
-        DB::commit();
+            DB::commit();
 
-            $response = [
-                'status' => 201,
-                'message' => 'Dokumen Berhasil!',
-                
-            ];
+                $response = [
+                    'status' => 201,
+                    'message' => 'Dokumen Berhasil!',
+                    
+                ];
 
-            return response()->json($response, 201);
+                return response()->json($response, 201);
 
-    }catch(\Exception $e){
-        DB::rollBack();
-            \Sentry\captureException($e);
-            $response = [
-                'status' => 400,
-                'message' => 'Ada error pada saat menambahkan data Dokumen',
-                'error' => $e->getMessage()
-            ];
-            return response()->json($response, 400);
+        }catch(\Exception $e){
+            DB::rollBack();
+                \Sentry\captureException($e);
+                $response = [
+                    'status' => 400,
+                    'message' => 'Ada error pada saat menambahkan data Dokumen',
+                    'error' => $e->getMessage()
+                ];
+                return response()->json($response, 400);
 
-    };
-
-
+        };
     }
 
     public function delete($id){
@@ -266,43 +262,28 @@ class DocumentController extends Controller
         }
     }
 
-    
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    
     public function show($id)
     {
-        try {
-
-            
+        try {           
             $data = Document::with([
-            'document_considers',
-            'document_remembers',
-            'document_notices',
-            'document_statuses',
-            'document_decisions',
-            'document_attachments',
-            'document_supports'])
-            ->where('id', $id)
-            ->firstOrFail();
+                'document_considers',
+                'document_remembers',
+                'document_notices',
+                'document_statuses',
+                'document_decisions',
+                'document_attachments',
+                'document_supports'])
+                ->where('id', $id)
+                ->firstOrFail();
             
             if($data!=null){
-
                 $response = [
                     'status' => 200,
                     'message' => 'Data Dokumen ditemukan',
-                    'data' => $data,
-                    
+                    'data' => $data, 
                 ];
                 return response()->json($response,200);
             }
-
-            
-
         } catch (\Exception $e) {
             \Sentry\captureException($e);
             $response = [
@@ -314,8 +295,6 @@ class DocumentController extends Controller
         }
     }
 
-
-    
     public function update(Request $request, $id)
     {
         $this->validate($request,[
@@ -352,13 +331,10 @@ class DocumentController extends Controller
             'document_attachments.*.margin_bottom' => 'required|integer',
             'document_attachments.*.margin_left' => 'required|integer',
             'document_attachments.*.margin_right' => 'required|integer'
-
-
         ]);
 
         try {
             DB::beginTransaction();
-
             $doc = Document::find($id);
 
             if ($doc) {
@@ -372,7 +348,6 @@ class DocumentController extends Controller
                 if (!empty($request->get('document_considers'))) {
                     DocumentConsider::where('document_id', $doc->id)->delete();
                     foreach ($request->get('document_considers') as $d) {
-                
                         $consider = new DocumentConsider;
                         $consider->document_id = $doc->id;
                         $consider->description = $d['description'];
@@ -387,7 +362,6 @@ class DocumentController extends Controller
                 if (!empty($request->get('document_remembers'))) {
                     DocumentRemember::where('document_id', $doc->id)->delete();
                     foreach ($request->get('document_remembers') as $d) {
-                
                         $remember = new DocumentRemember();
                         $remember->document_id = $doc->id;
                         $remember->description = $d['description'];
@@ -402,7 +376,6 @@ class DocumentController extends Controller
                 if (!empty($request->get('document_notices'))) {
                     DocumentNotice::where('document_id', $doc->id)->delete();
                     foreach ($request->get('document_notices') as $d) {
-                
                         $notice = new DocumentNotice;
                         $notice->document_id = $doc->id;
                         $notice->description = $d['description'];
@@ -417,7 +390,6 @@ class DocumentController extends Controller
                 if (!empty($request->get('document_attachments'))) {
                     DocumentAttachment::where('document_id', $doc->id)->delete();
                     foreach ($request->get('document_attachments') as $d) {
-                
                         $attachment = new DocumentAttachment();
                         $attachment->document_id = $doc->id;
                         $attachment->tittle = $d['tittle'];
@@ -433,7 +405,6 @@ class DocumentController extends Controller
                 if (!empty($request->get('document_supports'))) {
                     DocumentSupport::where('document_id', $doc->id)->delete();
                     foreach ($request->get('document_supports') as $d) {
-                
                         $suport = new DocumentSupport();
                         $suport->document_id = $doc->id;
                         $suport->file = $d['file'];    
@@ -451,16 +422,12 @@ class DocumentController extends Controller
                     $status->save();
                 }
 
-                
-
                 DB::commit();
-
                 $response = [
                     'status' => 200,
                     'message' => 'Data Document Behasil di update',
                     
                 ];
-
                 return response()->json($response, 200);
             }
             $response = [
@@ -469,7 +436,6 @@ class DocumentController extends Controller
             ];
 
             return response()->json($response, 404);
-
         } catch (\Exception $e) {
             DB::rollBack();
             \Sentry\captureException($e);
@@ -480,48 +446,64 @@ class DocumentController extends Controller
             ];
             return response()->json($response, 400);
         }
-
-        
-
-
     }
 
-   public function approveAdmin(Request $request){
-    try{
-        DB::beginTransaction();
-        $doc = Document::find($request->input('document_id'));
-
-        if(!$doc){
-
+   public function approveAdmin(Request $request) {
+        try {
+            DB::beginTransaction();
             
-        }
-        $doc->status = $request->input('status');
-        $status = new DocumentStatus;
-        $status->user_id =  Auth::user()->id;
-        $status->document_id = $request->input('document_id');
-        $status->status = $request->input('status');
-        $status->remark = $request->input('remark');
-        $status->save();    
-        
-        if($request->input('status')=='proses'){
-            ///bagian 3 nya masi blm ngerti 
-            $documents = Document::select(DB::raw('COUNT(*) as total_document, legal_drafter'))
-                    ->groupBy('legal_drafter')
-                    ->orderByRaw('COUNT(*) ASC') 
-                    ->first();
+            $doc = Document::find($request->input('document_id'));
+            $doc->status = $request->input('status');
 
-            $doc->legal_drafter = $documents['legal_drafter'];
-            //4
-            $doc->admin_verified = Auth::user()->id;
-            // untuk waktu masih ngebug
-            $doc->admin_verified_at = Carbon::now();
+            $status = new DocumentStatus;
+            $status->user_id =  Auth::user()->id;
+            $status->document_id = $request->input('document_id');
+            $status->status = $request->input('status');
+            $status->remark = $request->input('remark');
+            $status->save();    
             
+            if($request->input('status')=='PROSES'){
+                $documents = Document::select(DB::raw('COUNT(*) as total_document, legal_drafter'))
+                        ->groupBy('legal_drafter')->get();
 
-        }
+                return $documents;
+                        
+                if (count($documents) == 1) {
+                    // case belum ada sama sekali
+                    if (empty($documents[0]->legal_drafter)) {
+                        // ambil user legal drafter paling pertama 
+                        $legal_drafter = User::where('verificator', 2)->first();
+                    } else {
+                        // kalo ada ambil first except yang sudah ada
+                        $legal_drafter = User::where('verificator', 2)->where('id', '!=', $documents[0]->legal_drafter)->first();
+                    }
+                } else {
+                    $legal_drafters = [];
+                    foreach ($documents as $key => $document) {
+                        if (!empty($document->legal_drafter)) {
+                            array_push($legal_drafters, $document->legal_drafter);
+                        }
+                    }
+                    $legal_drafter = User::where('verificator', 2)
+                        ->whereNotIn('id', $legal_drafters)
+                        ->first();
+                    if (!$legal_drafter) {
+                        $documents = $documents->toArray();
+                        $object = array_reduce($documents, function($a, $b){
+                            return ($a['total_document'] < $b['total_document']) && (!empty($a['legal_drafter']))  ? $a : $b;
+                        }, array_shift($documents));
+                        $legal_drafter = User::where('verificator', 2)->where('id', $object['legal_drafter'])->first();                 
+                    }
+                }
 
-        $doc->save();
-        $status->save(); 
-        DB::commit();
+                $doc->legal_drafter = $legal_drafter->id;
+                $doc->admin_verified = Auth::user()->id;
+                $doc->admin_verified_at = Carbon::now();
+            }
+
+            $doc->save();
+            $status->save(); 
+            DB::commit();
 
             $response = [
                 'status' => 201,
@@ -530,20 +512,17 @@ class DocumentController extends Controller
             ];
 
             return response()->json($response, 201);
+        } catch(\Exception $e){
+            DB::rollBack();
+                \Sentry\captureException($e);
+                $response = [
+                    'status' => 400,
+                    'message' => 'Ada error saat update Data',
+                    'error' => $e->getMessage()
+                ];
+                return response()->json($response, 400);
 
-        
-
-    }catch(\Exception $e){
-        DB::rollBack();
-            \Sentry\captureException($e);
-            $response = [
-                'status' => 400,
-                'message' => 'Ada error saat update Data',
-                'error' => $e->getMessage()
-            ];
-            return response()->json($response, 400);
-
-    };
+        };
    }
 
    public function approveLegalDrafter(Request $request){
